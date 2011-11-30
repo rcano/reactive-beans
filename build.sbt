61ae23f28f67819feb3e770acd68c728706e7d8d
Rodrigo Cano <ioniviil@gmail.com>
2011-11-30 01:30:06 -0200

    Setup sbt0.11.

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "Reactive Beans"
+
+organization := "org.reactivebeans"
+
+version := "1.1"
+
+libraryDependencies <++= scalaVersion {sv => Seq(
+  "org.scala-lang" % "scala-swing" % sv,
+  "org.scala-lang" % "scalap" % sv
+)}
---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._, Keys._
+
+object Build extends sbt.Build {
+  lazy val reactivebeans = Project("reactivebeans", file(".")) settings(generateWrappersSettings)
+  lazy val generatedWrappersTester = Project("generatedWrappersTester", file("generatedWrappersTester"))
+
+
+  val generateWrappers = TaskKey[Unit]("generate-wrappers", "Generates wrappers into the test project")
+  val generateWrappersSettings = generateWrappers <<= (fullClasspath in Compile, mainClass in Compile, runner in run, streams) map {(cp, mc, runner, s) =>
+    if (mc.isEmpty) throw new IllegalStateException("No main-class defined")
+    runner.run(mc.get, cp map (_.data), Seq("-pn", "reactive.jswing",
+      "-bd", "generatedWrappersTester/src/main/scala/reactive/jswing",
+      "--bean-fixes", "defaultbeanfixes.*", "javax.swing.*"), s.log)
+    runner.run(mc.get, cp map (_.data), Seq("-pn", "reactive.sswing",
+      "-bd", "generatedWrappersTester/src/main/scala/reactive/sswing",
+      "--bean-fixes", "defaultbeanfixes.*", "scala.swing.*"), s.log)
+  }
+}
